ab1edbb18ed0819a678fe3636b5d3aa58e4f0745
timothysaatum <tsaatum@outlook.com>
2023-02-21 02:05:55 +0000

    push

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() = RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	if ((n % 10) > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
@@ -22,7 +22,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is zero\n", n, n % 0);
+		printf("Last digit of %d is %d and is zero\n", n, n % 10);
 	}
 	return (0);
 }
