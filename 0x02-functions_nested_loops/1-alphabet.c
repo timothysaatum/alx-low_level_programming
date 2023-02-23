a3d3c51b5b97a1076143bf8f4df7eb279e62beb2
timothysaatum <tsaatum@outlook.com>
2023-02-23 05:17:12 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_ alphabet - print all alphabet in lowercase
+ */
+void print_ alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_ putchar(letter);
+	_ putchar('\n');
+}
