e9eb6595233f4b9ba487154070e7a892ff23339a
timothysaatum <tsaatum@outlook.com>
2023-02-23 05:12:14 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - print _ putchar
+ * is written by mbah
+ * Return: 0
+ */
+int main(void)
+{
+	_ putchar('_ ');
+	_ putchar('p');
+	_ putchar('u');
+	_ putchar('t');
+	_ putchar('c');
+	_ putchar('h');
+	_ putchar('a');
+	_ putchar('r');
+	_ putchar('\n');
+	return (0);
+}
