8c036f616356a5bea8d26ebc47dd6abbef2246b5
timothysaatum <tsaatum@outlook.com>
2023-02-28 14:03:46 +0000

    101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -13,7 +14,6 @@ int main(void)
 
 	sum = 0;
 	srand(time(NULL));
-
 	for (i = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
